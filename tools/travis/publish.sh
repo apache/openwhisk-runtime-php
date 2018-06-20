e44ce2e02eb8561d95d2d61afa4fa556de5a6f91
Carlos Santana <csantanapr@apache.org>
2018-06-20 14:41:14 -0400

    fix travis publish for 7.2

--- tools/travis/publish.sh
+++ tools/travis/publish.sh
@@ -30,19 +30,13 @@ IMAGE_PREFIX=$1
 RUNTIME_VERSION=$2
 IMAGE_TAG=$3
 
-if [ ${RUNTIME_VERSION} == "7" ]; then
-  RUNTIME="php7Action"
-elif [ ${RUNTIME_VERSION} == "7.1" ]; then
-  RUNTIME="php7.1Action"
-fi
-
 if [[ ! -z ${DOCKER_USER} ]] && [[ ! -z ${DOCKER_PASSWORD} ]]; then
 docker login -u "${DOCKER_USER}" -p "${DOCKER_PASSWORD}"
 fi
 
 if [[ ! -z ${RUNTIME} ]]; then
 TERM=dumb ./gradlew \
-:core:${RUNTIME}:distDocker \
+:core:php${RUNTIME}Action:distDocker \
 -PdockerRegistry=docker.io \
 -PdockerImagePrefix=${IMAGE_PREFIX} \
 -PdockerImageTag=${IMAGE_TAG}
