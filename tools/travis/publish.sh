4e96e30ba2b585767db17f56831764ecaeff587c
David Grove <dgrove-oss@users.noreply.github.com>
2019-06-29 17:56:04 -0400

    change travis builds to publish docker images with tag 'nightly' (#63)

--- tools/travis/publish.sh
+++ tools/travis/publish.sh
@@ -41,8 +41,8 @@ TERM=dumb ./gradlew \
 -PdockerImagePrefix=${IMAGE_PREFIX} \
 -PdockerImageTag=${IMAGE_TAG}
 
-  # if doing latest also push a tag with the hash commit
-  if [ ${IMAGE_TAG} == "latest" ]; then
+  # if doing nightly also push a tag with the hash commit
+  if [ ${IMAGE_TAG} == "nightly" ]; then
   SHORT_COMMIT=`git rev-parse --short HEAD`
   TERM=dumb ./gradlew \
   :core:php${RUNTIME_VERSION}Action:distDocker \
